e1e1f56cd158891f79079b44aff2101dc3b59d8f
Alexis Clément <clemalex20@gmail.com>
2020-03-20 09:25:47 +0100

    choix factices

--- MonumentQuizz/quizz/constants.py
+++ MonumentQuizz/quizz/constants.py
@@ -1,2 +1,8 @@
 EXPERIENCE_CONSTANT = 10
 EXPERIENCE_EXP_INCREASE = 0.2
+
+FAKE_CITIES_LIST = [
+    'Paris', 'Chicago', 'Berlin', 'Beyrouth', 'Le Caire', 'Moscou', 'New York', 'Sydney',
+    'Rio de Janeiro', 'Lima', 'Angkor', 'Pekin', 'Tokyo', 'Calcutta', 'Singapore', 'Toronto',
+    'Grenoble', 'Rome', 'Madrid', 'Oslo',
+]
