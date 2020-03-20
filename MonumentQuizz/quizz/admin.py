4a3eb9bd25e5f1078e41b82112ad0a7572d6a1af
Alexis Clément <clemalex20@gmail.com>
2020-03-20 11:21:54 +0100

    base de donnée

--- MonumentQuizz/quizz/admin.py
+++ MonumentQuizz/quizz/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import QuizzUser
 from .models import Monument
+from .models import UserMonument
 
 admin.site.register(QuizzUser)
 admin.site.register(Monument)
+admin.site.register(UserMonument)
